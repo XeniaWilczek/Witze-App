78d2b58a3f24651d0587fa40bc7ece628ca475b3
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-03-06 16:04:16 +0100

    Start additional task

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,12 @@
+const developerJokes =
+  "https://witzapi.de/api/joke/?limit=1&category=programmierwitze&language=de";
+const trickQuestions =
+  "https://witzapi.de/api/joke/?limit=1&category=scherzfragen&language=de";
+const schoolJokes =
+  "https://witzapi.de/api/joke/?limit=1&category=schulwitze&language=de";
+
 export async function getJoke() {
-  const response = await fetch(
-    "https://witzapi.de/api/joke/?limit=1&category=programmierwitze&language=de",
-  );
+  const response = await fetch(selectedCategory);
   const joke = await response.json();
   let currentJoke = {
     id: Math.random(),
